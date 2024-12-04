90cd50203676d466eaa45d01ced0f4ecb110fdfc
Ali <alihakimi014@gmail.com>
2024-12-03 20:50:06 -0700

    Contact, footer, scroll reveal, dimensions

--- assets/js/main.js
+++ assets/js/main.js
@@ -48,7 +48,7 @@ const sendEmail = (e) => {
             contactMessage.textContent = 'Message send successfully ✅'
 
             //Remove message after five seconds
-            setTimeout(() =>{
+            setTimeout(() => {
                 contactMessage.textContent = ''
             }, 5000)
 
@@ -61,9 +61,45 @@ const sendEmail = (e) => {
 
 contactForm.addEventListener('submit', sendEmail)
 /*=============== SHOW SCROLL UP ===============*/
-
+const scrollUp = () => {
+    const scrollUp = document.getElementById('scroll-up')
+    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+        : scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+    
+const scrollActive = () =>{
+  	const scrollDown = window.scrollY
 
+	sections.forEach(current =>{
+		const sectionHeight = current.offsetHeight,
+			  sectionTop = current.offsetTop - 58,
+			  sectionId = current.getAttribute('id'),
+			  sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+		if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+			sectionsClass.classList.add('active-link')
+		}else{
+			sectionsClass.classList.remove('active-link')
+		}                                                    
+	})
+}
+window.addEventListener('scroll', scrollActive)
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+})
+
+sr.reveal(`.home__data, .home__social, .contact__container, .footer__container`)
+sr.reveal(`.home__image`, {origin: 'bottom'})
+sr.reveal(`.about__data, .skills__data`, {origin: 'left'})
+sr.reveal(`.about__image, .skills__content`, {origin: 'right'})
+sr.reveal(`.careers__card, .projects__card`, {interval: 100})
